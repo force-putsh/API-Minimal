0a126f68e1b948396b816974e0e7467a94df298b
victor perez ngounou <vngounou26@gmail.com>
2022-10-30 15:56:50 +0100

    🥳 Finish #3

--- PizzaStoreEF/Program.cs
+++ PizzaStoreEF/Program.cs
@@ -1,12 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PizzaStoreEF.Context;
+using PizzaStoreEF.DTA;
+using PizzaStoreEF.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<PizzaDbContext>(options => options.UseInMemoryDatabase("items"));
+
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
+builder.Services.AddEndpointsApiExplorer(); builder.Services.AddSqlite<PizzaDbContext>(connectionString);
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new() {
@@ -24,6 +28,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//Add Routes to the pipeline
+app.MapGet("/pizzas", async ([FromBody]PizzaRepository repository) => await repository.GetPizzasAsync());
+app.MapGet("/pizzas/{id}", async (int id, [FromBody] PizzaRepository repository) => await repository.GetPizzaAsync(id));
+//app.MapPost("/pizzas", async (Pizza pizza, PizzaRepository repository) => await repository.AddPizzaAsync(pizza));
+//app.MapPut("/pizzas/{id}", async (int id, Pizza pizza, [FromBody] PizzaRepository repository) => await repository.UpdatePizzaAsync(pizza));
+app.MapDelete("/pizzas/{id}", async (int id, [FromBody] PizzaRepository repository) => await repository.DeletePizzaAsync(id));
 
 
 app.Run();
