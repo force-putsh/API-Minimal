660fc1600d61c8658afe760cd988aa91aaeee4a3
victor perez ngounou <vngounou26@gmail.com>
2022-10-30 15:13:02 +0100

    🤡 Ajout de EF #3

---
+++ PizzaStoreEF/Context/PizzaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaStoreEF.Models;
+
+namespace PizzaStoreEF.Context;
+
+public class PizzaDbContext:DbContext
+{
+    public PizzaDbContext(DbContextOptions<PizzaDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Pizza> Pizzas { get; set; } = null!;
+
+}
---
+++ PizzaStoreEF/Models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaStoreEF.Models;
+
+public class Pizza
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+}
